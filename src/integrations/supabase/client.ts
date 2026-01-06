ae5ea4b6fd4335493a10ca82e5fee22c53987b9b
DOUXDOUX84 <douxdoux84@gmail.com>
2026-01-06 13:35:35 +0100

    Connect to new Supabase project CHAKEOFF-CHEZMAMINABIO

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://csfpfatfknjzednaypbx.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzZnBmYXRma25qemVkbmF5cGJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUyNjE2MTIsImV4cCI6MjA1MDgzNzYxMn0.3iaaUtdONsdSkrkdNrtjVJNxv3JdPPf8O-t9fplcgSs";
+const SUPABASE_URL = "https://ohejeaaptzknnwgwimdy.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oZWplYWFwdHprbm53Z3dpbWR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2OTY1MzMsImV4cCI6MjA4MzI3MjUzM30.D8jOsv7NVpAMXlEzLM7NRkWApr7GkoEKW354W0x6pIo";
 
 export const supabase = createClient<Database>(
   SUPABASE_URL,
